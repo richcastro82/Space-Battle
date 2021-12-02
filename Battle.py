b5321f461ba4076ce3a13613818850f097ab93bb
richcastro82 <rich.castro@achesongroup.com>
2021-12-01 22:05:44 -0500

    Working out the Classes

--- Battle.py
+++ Battle.py
@@ -3,7 +3,7 @@
 
 import sys, pygame, os, random
 pygame.init()
-width=1000
+width=1200
 height=800
 size=(width, height)
 fps=60
@@ -12,7 +12,14 @@ gameScreen=pygame.display.set_mode(size)
 
 
 # IMPORT THE SHIP GRAPHICS
+RedShipGraphic=pygame.image.load("RedShip.png")
+BlueShipGraphic=pygame.image.load("BlueShip.png")
 
+
+
+class BUTTON:
+    def __init__(self, x, y, size, color):
+        pass
 
 
 
@@ -25,8 +32,9 @@ class SHIPS:
         self.ShipGraphic=None
         self.ShipLaser=None
 
-    def drawShip(self, ShipGraphic, ShipLaser):
-        pass
+    def drawShip(self, ShipGraphic):
+        shipRect=pygame.Rect(self.x, self.y, self.size, self.size)
+        gameScreen.blit(ShipGraphic, shipRect)
 
     def drawLaser(self):
         pass
@@ -34,17 +42,23 @@ class SHIPS:
 
 class GAME:
     def __init__(self):
-        ship=SHIPS()
+        RedShip=SHIPS(400, 175, 50)
+        BlueShip=SHIPS(400, 875, 50)
 
     def pause(self):
         pass
 
     def startMenu(self):
-        gameScreen.fill((255,0,0))
+        gameScreen.fill((100,100,100))
+        RedShip=SHIPS(1000, 400, 50)
+        BlueShip=SHIPS(100, 400, 50)
+        RedShip.drawShip(RedShipGraphic)
+        BlueShip.drawShip(BlueShipGraphic)
 
     def runGame(self):
         pass
         # DRAW GAME WINDOW
+
         # DRAW LEFT SHIP
         # DRAW RIGHT SHIP
         # CONTROL FOR BOTH SHIPS
@@ -66,7 +80,7 @@ def main():
 
         # GAME RUNTIME HERE
         RUNTIME.startMenu()
-        RUNTIME.runGame()
+        # RUNTIME.runGame()
 
         pygame.display.update()
 
